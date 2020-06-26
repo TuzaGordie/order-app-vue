8901d37a7c63e965155fb3767fde887fc7c23642
XodTuza <gordie2u@gmail.com>
2020-06-26 03:38:33 +0100

    initial vue setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
+import createPersistedState from 'vuex-persistedstate';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
   },
   mutations: {
@@ -12,4 +14,7 @@ export default new Vuex.Store({
   },
   modules: {
   },
+  plugins: [
+    createPersistedState(),
+  ],
 });
