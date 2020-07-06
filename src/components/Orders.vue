5d187acd7254745b24676f757a2983b7d3cdb4ba
XodTuza <gordie2u@gmail.com>
2020-07-07 00:41:48 +0100

    edit and delete orders

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,9 +1,32 @@
 <template>
   <Panel title="Orders">
-    <div v-for="order in orders" :key="order.id">
-      {{ order.name }}
+    <div
+    class="order mt-2 pl-2"
+    v-for="order in orders"
+    :key="order.id"
+    >
+      <v-layout row wrap>
+        <v-flex xs9 class="text-xs-left">
+            <span v-if="!order.isEditMode">
+              {{ order.name }}
+            </span>
+            <v-text-field
+            autofocus
+            v-if="order.isEditMode"
+            :value="order.name"
+            @keyup.enter="saveOrder(order)"
+            @input="setOrderName({order, name: $event,})"
+            ></v-text-field>
+        </v-flex>
+        <v-flex xs3>
+          <v-icon v-if="!order.isEditMode" @click="setEditMode(order)">edit</v-icon>
+          <v-icon v-if="order.isEditMode" @click="saveOrder(order)">check</v-icon>
+          <v-icon @click="deleteOrder(order)">delete</v-icon>
+        </v-flex>
+      </v-layout>
     </div>
-    <v-text-field placeholder="Place an Order"
+    <v-text-field placeholder="Place an Order" class="mt-6"
+    @keyup.enter="createOrder"
     @input="setNewOrderName" :value="newOrderName"> </v-text-field>
     <v-btn color="blue" dark @click="createOrder">Create</v-btn>
   </Panel>
@@ -13,14 +36,42 @@
 import { mapMutations, mapState, mapActions } from 'vuex';
 
 export default {
+  mounted() {
+    this.fetchOrders();
+  },
   computed: {
     ...mapState('orders', ['newOrderName', 'orders']),
   },
   methods: {
-    ...mapMutations('orders', ['setNewOrderName']),
-    ...mapActions('orders', ['createOrder']),
+    ...mapMutations('orders', [
+      'setNewOrderName',
+      'setEditMode',
+      'setOrderName',
+    ]),
+    ...mapActions('orders', [
+      'createOrder',
+      'fetchOrders',
+      'saveOrder',
+      'deleteOrder',
+    ]),
   },
 };
 </script>
 
-<style></style>
+<style>
+.text-xs-left {
+  text-align: left;
+}
+
+.order {
+  font-size: 20px;
+}
+
+.icon {
+  cursor: pointer;
+}
+
+.icon:hover {
+  color: #333;
+}
+</style>
--- src/store/orders.js
+++ src/store/orders.js
@@ -1,4 +1,5 @@
 // import router from '../router';
+import Vue from 'vue';
 // eslint-disable-next-line import/no-cycle
 import HTTP from '../http';
 
@@ -9,6 +10,24 @@ export default {
     newOrderName: null,
   },
   actions: {
+    saveOrder({ commit }, order) {
+      return HTTP().patch(`orders/${order.id}`, order)
+        .then(() => {
+          commit('unsetEditMode', order);
+        });
+    },
+    deleteOrder({ commit }, order) {
+      return HTTP().delete(`orders/${order.id}`)
+        .then(() => {
+          commit('removeOrder', order);
+        });
+    },
+    fetchOrders({ commit }) {
+      return HTTP().get('/orders')
+        .then(({ data }) => {
+          commit('setOrders', data);
+        });
+    },
     createOrder({ commit, state }) {
       return HTTP().post('/orders', {
         name: state.newOrderName,
@@ -28,6 +47,21 @@ export default {
     },
     appendOrder(state, order) {
       state.orders.push(order);
+    },
+    setOrders(state, orders) {
+      state.orders = orders;
+    },
+    setOrderName(state, { order, name }) {
+      order.name = name;
+    },
+    setEditMode(state, order) {
+      Vue.set(order, 'isEditMode', true);
+    },
+    unsetEditMode(state, order) {
+      Vue.set(order, 'isEditMode', false);
+    },
+    removeOrder(state, order) {
+      state.orders.splice(state.orders.indexOf(order), 1);
     },
   },
 };
