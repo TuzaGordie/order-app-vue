a7b380b89f168c5d9f0afbfc0820462ad76c118d
XodTuza <gordie2u@gmail.com>
2020-07-12 13:11:28 +0100

    fixes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3333',
+        target: 'https://simple-order-api.herokuapp.com/',
       },
     },
   },
