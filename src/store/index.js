fcfab23c11ad51cb20acbbd8f9219d721b39d297
XodTuza <gordie2u@gmail.com>
2020-07-13 01:08:25 +0100

    fixing.

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: true,
   state: {
-    baseUrl: 'simple-order-app.herokuapp.com',
+    baseUrl: '/api',
   },
   modules: {
     authentication,
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   devServer: {
+    disableHostCheck: true,
     proxy: {
       '/api': {
-        target: 'https://simple-order-api.herokuapp.com/',
+        target: 'https://simple-order-api.herokuapp.com',
       },
     },
   },
