17495cacb0269d86fe52d1c162e8e838bb3c05aa
XodTuza <gordie2u@gmail.com>
2020-07-14 22:10:38 +0100

    nginx.conf

--- src/http/index.js
+++ src/http/index.js
@@ -4,7 +4,7 @@ import store from '../store';
 
 export default () => {
   return axios.create({
-    baseURL: 'https://simple-order-api.herokuapp.com',
+    baseURL: store.state.baseUrl,
     timeout: 5000,
     headers: {
       Authorization: `Bearer ${store.state.authentication.token}`,
